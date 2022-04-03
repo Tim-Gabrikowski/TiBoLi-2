7dafd84e3134c62faf9fc2f72fa602090f107db1
TimsGitHubProjects <timgabrikowski@outlook.de>
2022-04-03 16:34:49 +0200

    Update index.js

--- settings/index.js
+++ settings/index.js
@@ -3,9 +3,11 @@ const settings = require("../appSettings.json");
 const levensthein = require("js-levenshtein");
 
 router.post("/login", (req, res) => {
-	var dist = levensthein(req.body.password, settings.login.password);
+	const input = `${req.body.password}`;
 
+	var dist = levensthein(input, settings.login.password);
 	console.log("Login?", req.body, "Distance:", dist);
+
 	if (req.body.password == settings.login.password) {
 		console.log("ok");
 		res.status(200).send({ ok: true });
