d421258976f880e4e898cf8e0f8912a6de11edc0
TimsGitHubProjects <timgabrikowski@outlook.de>
2022-04-04 14:05:33 +0200

    Update index.js

--- settings/index.js
+++ settings/index.js
@@ -6,12 +6,14 @@ router.post("/login", (req, res) => {
 	const input = `${req.body.password}`;
 
 	var dist = levensthein(input, settings.login.password);
-	console.log("Login?", req.body, "Distance:", dist);
 
 	if (req.body.password == settings.login.password) {
+		console.log("Login?", "{ password: '*********'", "Distance:", dist);
 		console.log("ok");
 		res.status(200).send({ ok: true });
 	} else {
+		console.log("Login?", req.body, "Distance:", dist);
+		console.log("no.");
 		res.send({ ok: false });
 	}
 });
